52b15e3d4d50fb11b3432bf1cb2b819ea910a541
Getnet Bogale <getnetbogale27@gmail.com>
2024-10-19 14:48:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,26 +6,26 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# @st.cache_data
-# def load_data():
-#     url = 'https://raw.githubusercontent.com/getnetbogale27/Baacumen-Unsupervised-Learning-Project/refs/heads/main/Dataset/Online_Retail_dataset.csv'
-#     df = pd.read_csv(url)
-#     return df
-
 @st.cache_data
 def load_data():
     url = 'https://raw.githubusercontent.com/getnetbogale27/Baacumen-Unsupervised-Learning-Project/refs/heads/main/Dataset/Online_Retail_dataset.csv'
     df = pd.read_csv(url, encoding='ISO-8859-1')  # Use ISO encoding
     return df
 
-
 # Load the data
 df = load_data()
 
 # Streamlit app title
-st.title('🤖 Machine Learning App')
+st.title('🤖 Unsupervised Machine Learning App')
 
-st.info('This app explores child undernutrition data and builds a predictive machine learning model.')
+st.info(
+    "An online retailer wants to understand its customer base and their purchasing patterns. "
+    "The provided dataset contains transactional data, including purchase amounts, frequency, and customer information. "
+    "Your objective is to segment the customers using K-means clustering based on their transaction behavior "
+    "(e.g., purchase frequency, spending amount, and recency). Use the RFM (Recency, Frequency, Monetary) model as part of your clustering process "
+    "and provide insights into customer loyalty and behavior for targeted marketing strategies."
+)
+
 
 # Expanders for different data views
 with st.expander('🔢 Raw data (first 5 rows)'):
