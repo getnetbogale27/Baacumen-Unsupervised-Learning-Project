34875249d63e8df7e2257280f35dc679722fd530
Getnet Bogale <getnetbogale27@gmail.com>
2024-10-20 00:08:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ rfm.columns = ['CustomerID', 'Recency', 'Frequency', 'Monetary']
 # Apply log transformation to Monetary for skewness correction
 # rfm['Recency'] = np.log1p(rfm['Recency'])
 # rfm['Frequency'] = np.sqrt(rfm['Frequency'])
-# rfm['Monetary'] = np.log1p(rfm['Monetary'])
+rfm['Monetary'] = np.log1p(rfm['Monetary'])
 
 # Standardize RFM values
 scaler = StandardScaler()
